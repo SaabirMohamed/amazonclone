b60dd46ccd8b037558069ca89bcac8c33e42fb9c
Saabir Mohamed <saabir@Saabirs-MacBook-Air.local>
2021-05-23 18:30:02 +0200

    23-05-2021_18-30-02

--- vercelpublish.sh
+++ vercelpublish.sh
@@ -1,6 +1,7 @@
 [[ -z "$1" ]] && { echo "Error:please provide a message in quotes [./vercelpublish 'updated homepage css']"; exit 1; }
-TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S-$1`
 echo uploading... ---> git-repo ---> vercel
-git add . && git commit -m TIMESTAMP && git push
+TIMESTAMP=$(date +%d-%m-%Y_%H-%M-%S)
+echo $TIMESTAMP
+git add . && git commit -m $TIMESTAMP && git push
 echo done
 
